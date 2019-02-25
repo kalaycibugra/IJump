9629da760291b297ba2e2d638a0f2d2a4e5232bd
Bugra Kalayci <kalaycibugra@gmail.com>
2019-02-25 11:44:48 -0500

    simple object movement in progress

--- app/src/main/java/com/example/ijump/MainActivity.java
+++ app/src/main/java/com/example/ijump/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ijump;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.start);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent nextPage = new Intent(MainActivity.this,Game.class);
+                startActivity(nextPage);
+            }
+        });
     }
 }
