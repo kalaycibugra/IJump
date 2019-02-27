2b5d82aba469060644127d216a929df22fe1d73b
Bugra Kalayci <kalaycibugra@gmail.com>
2019-02-26 23:40:06 -0500

    Collusion Detection added

--- app/src/main/java/com/example/ijump/GameView.java
+++ app/src/main/java/com/example/ijump/GameView.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.graphics.Paint;
 
 import android.os.Handler;
 
@@ -18,6 +20,7 @@ import java.util.Random;
 public class GameView extends View {
     Handler handler;
     Runnable runnable;
+    boolean check[];
     final int UPDATE_MILLIS=50;
     Bitmap background;
     Display display;
@@ -34,10 +37,17 @@ public class GameView extends View {
     int velocity=0,gravity=8,block_vel=18;
     int cloud_vel=0;
     Random rand = new Random();
+    int coinCount=0;
+
+    Paint paint2;
+
     public GameView(Context context) {
 
         super(context);
 //    init();
+        paint2= new Paint();
+        paint2.setColor(Color.BLACK);
+        paint2.setTextSize(20);
         handler=new Handler();
         runnable=new Runnable() {
             @Override
@@ -46,7 +56,7 @@ public class GameView extends View {
             }
         };
         background= BitmapFactory.decodeResource(getResources(),R.drawable.background);
-        block=BitmapFactory.decodeResource(getResources(),R.drawable.block);
+        block=BitmapFactory.decodeResource(getResources(),R.drawable.bloc);
         display=((Activity)getContext()).getWindowManager().getDefaultDisplay();
         point=new Point();
         display.getSize(point);
@@ -54,14 +64,16 @@ public class GameView extends View {
         dWidth=point.x;
         rect = new Rect(0,0,dWidth,dHeight);
         man = new Bitmap[3];
+        check = new boolean[3];
         block_loc = new int[3];
         block_h = new int[3];
 //        block_loc[0]=dWidth-rand.nextInt(dWidth);
 //        block_loc[1]=dWidth-rand.nextInt(dWidth);
 //        block_loc[2]=dWidth-rand.nextInt(dWidth);
         for(int i=0;i<3;i++) {
+            check[i]=false;
             block_loc[i] = dWidth - rand.nextInt(dWidth);
-            block_h[i] = dHeight - rand.nextInt(dHeight);
+            block_h[i] = dHeight - rand.nextInt(dHeight-200);
         }
         cloud1=BitmapFactory.decodeResource(getResources(),R.drawable.cloud);
 //        cloud2=BitmapFactory.decodeResource(getResources(),R.drawable.cloud);
@@ -88,16 +100,38 @@ public class GameView extends View {
                 int num=rand.nextInt(100);
 
                 block_loc[i]=dWidth-num;
-                block_h[i]=dHeight-rand.nextInt(dHeight)-block.getHeight();            }
+                block_h[i]=dHeight-rand.nextInt(dHeight-200)-200;
+                check[i]=false;
+            }
+            if(manX<=block_loc[i]&&block_loc[i]<=manX+man[0].getWidth())
+            {
+                if(manY>=block_h[i]&&block_h[i]>=manY-man[0].getHeight()){
+                    check[i]=true;
+                    coinCount++;
+                }
+            }
+            else if(manX<=block_loc[i]+block.getWidth()/2&&block_loc[i]+block.getWidth()/2<=manX+man[0].getWidth()/2)
+            {
+                if(manY>=block_h[i]-block.getHeight()/2&&block_h[i]-block.getHeight()/2>=manY-man[0].getHeight()/2){
+                    check[i]=true;
+                    coinCount++;
+
+                }
+            }
         }
+        String tex="Score: ";
+        tex=tex+Integer.toString(coinCount);
         canvas.drawBitmap(background,null,rect,null);
         int num1=rand.nextInt(4);
         canvas.drawBitmap(cloud1,cloud_vel,200,null);
         canvas.drawBitmap(cloud1,cloud_vel-400,100,null);
         canvas.drawBitmap(cloud1,cloud_vel-1000,300,null);
-        canvas.drawBitmap(block,block_loc[0],block_h[0],null);
-        canvas.drawBitmap(block,block_loc[1],block_h[1],null);
-        canvas.drawBitmap(block,block_loc[2],block_h[2],null);
+        
+        for(int i=0;i<3;i++){
+            if(check[i]==false)
+                canvas.drawBitmap(block,block_loc[i],block_h[i],null);
+        }
+
         if(manFrame==0){
             manFrame=1;
         }
@@ -108,7 +142,7 @@ public class GameView extends View {
             manFrame=0;
         }
 
-        if(manY<=dHeight-man[0].getHeight()-man[0].getHeight()/2 || velocity < 0){
+        if(manY<=dHeight-man[0].getHeight() || velocity < 0){
             velocity+=gravity;
             manY+=velocity;
 
@@ -121,6 +155,9 @@ public class GameView extends View {
             cloud_vel=dWidth-1;
         }
         canvas.drawBitmap(man[manFrame],manX,manY,null);
+
+
+
         handler.postDelayed(runnable,UPDATE_MILLIS);
     }
 
